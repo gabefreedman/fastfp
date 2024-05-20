5cfa792ec65e95aa2c0d6beab6ce6e2de086783c
Gabe Freedman <gfreedman2108@comcast.net>
2024-05-20 10:45:17 -0500

    add relative import from utils.py

--- fastfp/fastfp.py
+++ fastfp/fastfp.py
@@ -15,6 +15,9 @@ import jax.numpy as jnp
 from jax.tree_util import register_pytree_node_class
 jax.config.update('jax_enable_x64', True)
 
+# relative imports
+from fastfp.utils import get_xCy
+
 @register_pytree_node_class
 class Fp_jax(object):
     """
@@ -34,16 +37,6 @@ class Fp_jax(object):
         self.Ts = Ts
         self.Nvecs = Nvecs
         self.sigmainvs = sigmainvs
-
-    @jax.jit
-    def get_xCy(self, Nvec, T, sigmainv, x, y):
-        # Get x^T C^{-1} y
-        Nx = jnp.array(x / Nvec)
-        Ny = jnp.array(y / Nvec)
-        TNx = jnp.dot(T.T, Nx)
-        TNy = jnp.dot(T.T, Ny)
-        xNy = jnp.dot(x.T, Ny)
-        return xNy - TNx @ sigmainv @ TNy
     
     @jax.jit
     def calculate_Fp(self, fgw, Nvecs, TNTs, Ts, sigmainvs, toas, residuals):
@@ -57,14 +50,14 @@ class Fp_jax(object):
             A = A.at[0, :].set(1 / fgw ** (1 / 3) * jnp.sin(2 * jnp.pi * fgw * toa))
             A = A.at[1, :].set(1 / fgw ** (1 / 3) * jnp.cos(2 * jnp.pi * fgw * toa))
 
-            ip1 = self.get_xCy(Nvec, T, sigmainv, A[0, :], resid)
-            ip2 = self.get_xCy(Nvec, T, sigmainv, A[1, :], resid)
+            ip1 = get_xCy(Nvec, T, sigmainv, A[0, :], resid)
+            ip2 = get_xCy(Nvec, T, sigmainv, A[1, :], resid)
             N = jnp.array([ip1, ip2])
 
-            M = M.at[0,0].set(self.get_xCy(Nvec, T, sigmainv, A[0,:], A[0,:]))
-            M = M.at[0,1].set(self.get_xCy(Nvec, T, sigmainv, A[0,:], A[1,:]))
-            M = M.at[1,0].set(self.get_xCy(Nvec, T, sigmainv, A[1,:], A[0,:]))
-            M = M.at[1,1].set(self.get_xCy(Nvec, T, sigmainv, A[1,:], A[1,:]))
+            M = M.at[0,0].set(get_xCy(Nvec, T, sigmainv, A[0,:], A[0,:]))
+            M = M.at[0,1].set(get_xCy(Nvec, T, sigmainv, A[0,:], A[1,:]))
+            M = M.at[1,0].set(get_xCy(Nvec, T, sigmainv, A[1,:], A[0,:]))
+            M = M.at[1,1].set(get_xCy(Nvec, T, sigmainv, A[1,:], A[1,:]))
 
             Minv = jnp.linalg.pinv(M)
             fstat += 0.5 * jnp.dot(N, jnp.dot(Minv, N))
