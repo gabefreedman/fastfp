1b0ee0841da1392b8dfb91921cfa9a903da409df
Gabe Freedman <gfreedman2108@comcast.net>
2024-11-12 17:07:27 -0600

    add main noise-marginalized Fp-stat class and red-noise container

--- fastfp/nmfp.py
+++ fastfp/nmfp.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+"""Noise-marginalized Fp-statistic class and its function dependencies,
+written in JAX to allow for batching over CW frequencies as well as
+multiple noise realization (or draws from an MCMC chain).
+
+Can be run on GPUs for accelerated Fp-statistic calculations.
 """
-Noise-marginalized Fp-statistic class and its function dependencies
-"""
-import scipy.constants as sc
+# relative imports
+import fastfp.constants as const
+from fastfp.utils import get_xCy
 
 # JAX imports
 import jax
@@ -11,20 +16,30 @@ from jax.tree_util import register_pytree_node_class
 
 jax.config.update("jax_enable_x64", True)
 
-# relative imports
-from fastfp.utils import get_xCy
-
-yr = sc.Julian_year
-fyr = 1.0 / yr
-
 
 @register_pytree_node_class
 class NMFP(object):
-    """
-    Noise-marginalized Fp-statistic
+    """Class for calculating the noise-marginalized Fp-statistic.
+    This takes the calculation of the Fp-statistic in Ellis,
+    Siemens, and Creighton (2012) and runs for multiple
+    realizations of the pulsar red noise in the PTA. This is
+    typically accomplished by taking random draws from an MCMC
+    chain.
+
+    The per-pulsar red-noise signals are kept in a set of separate
+    classes written as JAX Pytrees. This allows the red-noise covariance
+    update step of the noise-marginalization process to be JIT-compiled.
+
+    :param psrs: A list of :class:`enterprise.pulsar.Pulsar` objects
+        containing pulsar TOAs and residuals
+    :type psrs: list
+    :param rn_sigs: A list of :class:`fastfp.nmfp.RN_container` objects
+        containing the per-pulsar red-noise signals
+    :type rn_sigs: list
     """
 
     def __init__(self, psrs, rn_sigs):
+        """Constructor method"""
         self.psrs = psrs
         self.rn_sigs = rn_sigs
 
@@ -32,12 +47,22 @@ class NMFP(object):
         self.residuals = [psr.residuals for psr in psrs]
 
     def __call__(self, fgw, samples, Nvecs, Ts, TNTs):
-        """Callable method
-        """
+        """Callable method"""
         return self.calculate_nmfp(fgw, samples, Nvecs, Ts, TNTs)
 
     @jax.jit
     def _get_sigmas(self, pars, TNTs):
+        """Calculate :math:`\Sigma = B^{-1} + T^{T}N^{-1}T`, the
+        portion of the total noise covariance that changes during
+        updates of the red noise.
+
+        :param pars: Dictionary of noise parameters
+        :type pars: dict
+        :param TNTs: List of :math:`T^{T}N^{-1}T` matrices for each pulsar
+        :type TNTs: list
+        :return: :math:`\Sigma` matrix for each pulsar
+        :type: list
+        """
         sigmas = []
         for i, (rn_sig, TNT) in enumerate(zip(self.rn_sigs, TNTs)):
             phiinv = rn_sig.get_phiinv(pars)
@@ -46,6 +71,23 @@ class NMFP(object):
 
     @jax.jit
     def calculate_nmfp(self, fgw, samples, Nvecs, Ts, TNTs):
+        """Calculate Fp value for a given GW frequency and at a particular
+        set of per-pulsar red noise parameters.
+
+        :param fgw: Input GW frequency
+        :type fgw: float
+        :param samples: Dictionary of noise parameters
+        :type samples: dict
+        :param Nvecs: List of per-pulsar white-noise covariance matrices
+        :type Nvecs: list
+        :param Ts: List of per-pulsar basis matrices for Gaussian-process
+            signals
+        :type Ts: list
+        :param TNTs: List of :math:`T^{T}N^{-1}T` matrices for each pulsar
+        :type TNTs: list
+        :return: :math:`F_{p}` value
+        :rtype: float
+        """
         sigmas = self._get_sigmas(samples, TNTs)
         N = jnp.zeros(2)
         M = jnp.zeros((2, 2))
@@ -73,10 +115,12 @@ class NMFP(object):
         return fstat
 
     def tree_flatten(self):
+        """Method for flattening custom PyTree"""
         return (), (self.psrs, self.rn_sigs)
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
+        """Method for reconstructing custom PyTree"""
         return cls(*aux_data, *children)
 
 
@@ -85,6 +129,20 @@ class RN_container(object):
     """
     Container class for storing and updating per-pulsar
     red-noise covariance matrices
+
+    :param psr: A single pulsar data container
+    :type psr: :class:`enterprise.pulsar.Pulsar`
+    :param Ffreqs: Array of Fourier frequencies for red-noise
+        basis model. (Note: if not provided, frequencies will
+        be calculated using input pulsar's TOAs, and the calculation
+        will be later be JIT-compiled along with any FP-statistic
+        calculations, which will slow down runtime.)
+    :type Ffreqs: array-like, optional.
+    :param ncomps: Number of Fourier components for red-noise model
+    :type ncomps: int, optional
+    :param tm_marg: Flag for turning on the marginalizing of the
+        timing model
+    :type tm_marg: bool, optional
     """
 
     def __init__(self, psr, Ffreqs=None, ncomps=30, tm_marg=False):
@@ -97,8 +155,6 @@ class RN_container(object):
 
         if isinstance(Ffreqs, jax.Array):
             self.Ffreqs = Ffreqs
-        # this will add the _create_freqarray to jit-compilation
-        # which will slow things down
         else:
             self.Ffreqs = self._create_freqarray(psr, ncomps=ncomps)
 
@@ -110,6 +166,16 @@ class RN_container(object):
             self.phi_fn = self.get_phi_rn
 
     def _create_freqarray(self, psr, ncomps=30):
+        """Create array of Fourier frequencies for red-noise model. Uses
+        input pulsar to set Tspan.
+
+        :param psr: A single pulsar data container
+        :type psr: :class:`enterprise.pulsar.Pulsar`
+        :param ncomps: Number of Fourier components for red-noise model
+        :type ncomps: int, optional
+        :return: Array of Fourier frequencies
+        :rtype: array-like
+        """
         Tspan = jnp.max(psr.toas) - jnp.min(psr.toas)
         f = 1.0 * jnp.arange(1, ncomps + 1) / Tspan
         Ffreqs = jnp.repeat(f, 2)
@@ -117,13 +183,20 @@ class RN_container(object):
 
     @jax.jit
     def _powerlaw(self, pars):
+        """Power-law model for red-noise PSD.
+
+        :param pars: Dictionary of noise parameters
+        :type pars: dict
+        :return: Power-law model evaluated for input parameters
+        :rtype: array-like
+        """
         df = jnp.diff(jnp.concatenate((jnp.array([0]), self.Ffreqs[::2])))
         return (
             self.Ffreqs ** (-pars[self.rn_gam_name])
             * (10 ** pars[self.rn_A_name]) ** 2
             / 12.0
             / jnp.pi**2
-            * fyr ** (pars[self.rn_gam_name] - 3)
+            * const.fyr ** (pars[self.rn_gam_name] - 3)
             * jnp.repeat(df, 2)
         )
 
@@ -139,18 +212,34 @@ class RN_container(object):
 
     @jax.jit
     def update_phi(self, pars):
+        """Calculate red-noise prior covariance matrix.
+
+        :param pars: Dictionary of noise parameters
+        :type pars: dict
+        :return: Red-noise prior covariance matrix
+        :rtype: array-like
+        """
         return self.phi_fn(pars)
 
     @jax.jit
     def get_phiinv(self, pars):
+        """Calculate inverse of red-noise prior covariance matrix.
+
+        :param pars: Dictionary of noise parameters
+        :type pars: dict
+        :return: Inverse of red-noise prior covariance matrix
+        :rtype: array-like
+        """
         phi = self.update_phi(pars)
         return 1.0 / phi
 
     def tree_flatten(self):
+        """Method for flattening custom PyTree"""
         return (self.Ffreqs,), (self.psr, self.ncomps, self.tm_marg)
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
+        """Method for reconstructing custom PyTree"""
         psr, ncomps, tm_marg = aux_data
         (Ffreqs,) = children
         return cls(psr, Ffreqs, ncomps, tm_marg)
